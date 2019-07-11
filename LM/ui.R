6585617519c9f60eca3ba37f47fe904983b09f5b
TCornulier <48022119+TCornulier@users.noreply.github.com>
2019-07-11 13:15:36 +0100

    Update ui.R

--- LM/ui.R
+++ LM/ui.R
@@ -1,1 +1,44 @@
 
+# Shiny ui for LM
+
+library(shiny)
+
+source('global.R')
+
+# Define UI for the application
+
+shinyUI(pageWithSidebar(
+
+
+  # Application title
+  headerPanel("Fit a Linear Model: Galton's height data"),
+
+  sidebarPanel(
+
+      sliderInput(inputId = 'Intercept', label = 'Intercept',
+                 min = 0, max = 200, value = 100, step = 5),
+
+      sliderInput(inputId = 'Slope', label = 'Slope',
+                  min = -2, max = 2, value = 0, step = 0.05)
+
+#      radioButtons(inputId = 'prob_type',
+#                   label = 'Probability:',
+#                   choices = c('Lower tail ' = 'lower',
+#                               'Upper tail ' = 'upper'),
+#                   selected = 'upper')
+#
+  ),
+
+
+  mainPanel(
+    tabsetPanel(
+      tabPanel(title = 'Slope and intercept',
+               plotOutput(outputId = 'fitted.plot'),
+               plotOutput(outputId = 'RSE.plot'))
+    )
+  )
+
+
+))
+
+
