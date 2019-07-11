e457824f5b2a77e2d1f2606b05b86aa7fca9c801
TCornulier <48022119+TCornulier@users.noreply.github.com>
2019-07-11 13:59:55 +0100

    Update server.R

--- LM/server.R
+++ LM/server.R
@@ -1,1 +1,26 @@
 
+# Shiny server for fitting normal LM to Galton's data (subset)
+library(shiny)
+
+source('global.R')
+
+# Define server logic
+shinyServer(function(input, output) {
+
+      inputVal <- reactive({
+          list(Intercept= input$Intercept,
+               Slope= input$Slope)
+      })
+
+      output$fitted.plot <- renderPlot({
+          fn.fitted.plot(inputVal())
+      })
+
+
+      output$RSE.plot <- renderPlot({
+        fn.SSE.plot(inputVal())
+      })
+
+})
+
+
